d00ecaff80dc33f94f87a0677db46ad07265b59a
manuelpc97 <manuelpc97@unitec.edu>
2016-02-12 14:48:11 -0600

    Declara e imprime tableros

--- laboratorio4.cpp
+++ laboratorio4.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using std::cout;
 using std::cin;
@@ -6,15 +8,20 @@ using std::endl;
 
 void llenarMatriz(int***);
 void mostrarSubmarinos(int***);
-void declararMatriz(int***);
+int*** declararMatriz();
 int submarinosContrarios(int***);
 
 int main(int argc, char*argv[]){
+	srand(time(NULL));
 	bool seguir = true;
 	int decision = 0;
+	int*** tablero1 = declararMatriz();
+	int*** tablero2 = declararMatriz();
+		
 
 	while(seguir){
-
+	llenarMatriz(tablero1);
+	llenarMatriz(tablero2);
 
 
 
@@ -31,3 +38,35 @@ int main(int argc, char*argv[]){
 	}	
 return 0;
 }
+
+int*** declararMatriz(){
+	int***h=new int**[12];
+	
+	for(int i = 0; i<12; i++){
+		h[i]= new int*[12];		
+	}
+
+	for(j = 0; j<12;j++){
+		for(k=0;k<12;k++){
+			h[i][j] = new int[12];
+		}
+	}
+
+	return h;	
+
+}
+
+void llenarMatriz(int*** a){
+	int numeros[3];
+	int contador = 0;
+	while(contador<15){
+		for(int j = 0; j<3;j++){
+			numeros[j]= 1+rand()%(13-1);
+		}
+		if(a[numeros[0]][numeros[1]][numeros[2]]==0){
+			a[numeros[0]][numeros[1]][numeros[2]]=1;
+			contador++;
+		}
+	}
+}
+
